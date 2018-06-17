01dc5ea7fb370661f985869c42378513255b6f05
Filip Vitas <filip.vitas@sbgenomics.com>
2018-06-17 13:41:25 +0200

    add icon and start main window

--- main.js
+++ main.js
@@ -6,22 +6,21 @@ const menubar = require('menubar')
 const path = require('path')
 const url = require('url')
 
-// Let electron reloads by itself when parcel watches changes in ./app/
 if (process.env.ELECTRON_START_URL) {
     require('electron-reload')(__dirname)
 }
 
 const mb = menubar({
     alwaysOnTop: true,
-    showDockIcon: false,
-    index: process.env.ELECTRON_START_URL || url.format({ pathname: path.join(__dirname, './build/index.html'), protocol: 'file:', slashes: true }),
-    // icon: path.join(__dirname, 'icons/IconTemplate.png'),
+    showDockIcon: true,
+    index: process.env.ELECTRON_START_URL || url.format({ pathname: path.join(__dirname, 'build/index.html'), protocol: 'file:', slashes: true }),
+    icon: path.join(__dirname, 'src/main/assets/icons/IconTemplate.png'),
     width: 320,
     height: 650,
     y: 20,
     transparent: true,
     hasShadow: false,
-    resizable: true,
+    resizable: false,
     frame: false,
     showOnAllWorkspaces: true,
     useContentSize: true,
@@ -32,13 +31,5 @@ const mb = menubar({
 })
 
 mb.on('ready', function ready () {
-    if (process.env.NODE_ENV === 'development') {
-        mb.on('after-show', () => {
-            mb.window.openDevTools({mode: 'detach'})
-        })
 
-        mb.on('after-close', function () {
-            console.log('after closed')
-        })
-    }
 })
